e1264eb664e0e174a3cc8c2acac0b36bd3677aed
Budianto Tan <budianto.tan@traveloka.com>
2019-09-26 14:51:02 +0700

    Modify as executable

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
+#!/usr/bin/env node
+
 const fs = require('fs');
 const readline = require('readline');
 const qrcode = require('qrcode');
 const eachLimit = require('async/eachLimit');
 
+const cwd = process.cwd();
+const inputFilePath = process.argv[2] || 'input.txt';
+
 // Create output folder
-fs.mkdir(`${__dirname}/output`, { recursive: true }, (err) => {
+fs.mkdir(`${cwd}/output`, { recursive: true }, (err) => {
   if (err) throw err;
 });
 
 const texts = [];
 
 // Input File
-const inputFile = fs.createReadStream(`${__dirname}/input.txt`)
+const inputFile = fs.createReadStream(`${cwd}/${inputFilePath}`)
   .on('error', () => {
-    console.log('input.txt not found!')
+    console.log(`File '${inputFilePath}' not found!`)
     process.exit(0)
   });
 
@@ -25,7 +30,7 @@ readline
     console.log('Generating qr codes..');
 
     eachLimit(texts, 1, (text, callback) => {
-      qrcode.toFile(`${__dirname}/output/${text}.png`, text, {
+      qrcode.toFile(`${cwd}/output/${text}.png`, text, {
         errorCorrectionLevel: 'H'
       }, err => {
         if (err) throw err;
